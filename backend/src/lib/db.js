f1df49b563e5103b6d9828aa442a8a37bef7efb6
Manoj-Bendke <manojbendkev@gmail.com>
2025-12-06 15:42:58 +0530

    DB_URL validation changes

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,9 @@ import mongoose from 'mongoose';
 import {ENV} from "./env.js"
 export async function dbConnect(){
 try{
+  if(!ENV.DB_URL){
+    throw new Error("DB_URL not defined")
+  }
 const connect = await mongoose.connect(ENV.DB_URL);
 console.log("DB connected Successfully", connect.connection.host);
 
