16cb993e95b549cb1f62af76b0b7fbfe25603559
Manoj-Bendke <manojbendkev@gmail.com>
2025-12-08 17:21:08 +0530

    Server side auth implemented

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -5,7 +5,7 @@ const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
 
 if (!apiKey || !apiSecret) {
-  console.error("Stream api key or stream secret key is not presnt");
+  console.error("Stream api key or stream secret key is not present");
 }
 
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
---
+++ backend/src/middleware/protectRoute.js
@@ -0,0 +1,21 @@
+import { requireAuth } from "@clerk/express";
+import  {User}  from "../models/Users.js";
+export const protectRoute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
+      if (!clerkId) {
+        return res.status(401).json({ error: "user not authorized" });
+      }
+      const user = await User.findOne({clerkId});
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      res.status(400).json({error:"something went wrong with the authorization",error})
+    }
+  },
+];
