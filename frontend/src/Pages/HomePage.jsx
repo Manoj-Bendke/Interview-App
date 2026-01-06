b2b6f4d7192a6f53db0ee54f1d8e9ad550b5b71f
Manoj-Bendke <manojbendkev@gmail.com>
2026-01-06 10:54:33 +0530

    setup of frontend tools

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,23 @@
+  import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton,useUser } from "@clerk/clerk-react";
+  import {toast} from "react-hot-toast";
+  function HomePage(){
+    const isSignedIn = useUser();
+    return(
+      <>
+      <h1>Hii Welcome to the app</h1>
+      <SignedOut>
+          <SignInButton mode="modal" >
+            <button >Log in</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <SignOutButton mode="modal" >
+            <button>Log Out</button>
+          </SignOutButton>
+        </SignedIn> 
+        <UserButton/>
+        <button className="btn btn-secondary" onClick={()=> isSignedIn ? toast.success("This show toast is working") : toast.error("this toast did not work")}>checkSign in</button>
+        </>
+    )
+  }
+  export default HomePage;
